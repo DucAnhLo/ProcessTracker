9c43ff88af1d8744cf125ca30e31dc7c12c3e2b5
Liamml <42917027+Liamml@users.noreply.github.com>
2022-07-12 15:38:20 +0100

    Finish Update and Delete Routes for Post Route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -10,7 +10,9 @@ const Post = require('../models/Post')
 
 router.get('/',verfifyToken, async(req,res) => {
     try {
-        const posts = await Post.find({user: req.UserId})
+        const posts = await Post.find({user: req.UserId}).populate('user', [
+			'username'
+		])
         res.status(200).json({success: true, posts})
     } catch (error) {
         console.log(error)
@@ -46,6 +48,59 @@ router.post('/',verfifyToken, async(req,res) => {
 
 
 })
+
+
+//Update
+router.put('/:id',verfifyToken, async(req,res) => {
+    const {title, description, url, status, user} = req.body
+    if(!title)
+    return res.status(400).json({success: false, message:"Title is required"})
+
+    try {
+        let updatedPost = {
+            title,
+            description: description || '',
+            url: url || '',
+            status: status || 'TO LEARN'
+        }
+
+        const updateCondition = {_id: req.params.id, user: req.UserId}
+
+        updatedPost = await Post.findByIdAndUpdate(updateCondition, updatedPost, {new: true})
+
+        //Unauthorized user try to update or post not found 
+        if(!updateCondition)
+        return res.status(401).json({success: false, message:"User unauthorized to update post or post not found"})
+
+        res.status(200).json({success: true, message:"Update post successfully", post: updatedPost})
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({sucess: false, message:'Interval error'})
+    }
+
+})
+
+//Delete Post
+
+router.delete('/:id', verfifyToken, async(req,res) => {
+    try {
+        const deleteCondition = {_id: req.params.id, user:req.UserId}
+        const deletedPost = await Post.findByIdAndDelete(deleteCondition)
+
+        if(!deletedPost)
+        return res.status(401).json({success: false, message:"User unauthorized to update post or post not found"})
+
+        res.status(200).json({success: true, message:"Delete post successfully", post: deletedPost})
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({sucess: false, message:'Interval error'})
+    }
+})
+
 
 
 module.exports = router
